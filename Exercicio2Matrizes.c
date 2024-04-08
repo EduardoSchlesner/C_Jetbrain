936ca4559ef2a58500da3e829c8963101eea8a21
EduardoSchlesner <eduardoschlesner73@gmail.com>
2024-04-07 21:50:24 -0300

    Alterações

--- Exercicio2Matrizes.c
+++ Exercicio2Matrizes.c
@@ -1,8 +1,12 @@
+//Faça uma função que receba como parâmetro (matriz) as alturas de dez
+//atletas de 5 delegações que participarão das olimpíadas e imprima a maior
+//altura de cada delegação.
+
 #include<stdio.h>
 
 int main(){
 
-    printf("matriz");
+    printf("opa");
 
     return 0;
 }
